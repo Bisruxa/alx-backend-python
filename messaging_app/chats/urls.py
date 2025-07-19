b755d595e23fb3f4945e950294c0ad7010db3e98
Elkan <Elkan@GALAXY-BOOK>
2025-07-19 10:42:39 -0900

    urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,28 +1,28 @@
-from django.urls import path, include
-from rest_framework import routers
-from .views import ConversationViewSet, MessageViewSet
-
-router = routers.DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
-
-urlpatterns = [
-    path('', include(router.urls)),
-]
-
-
-
-
-# from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
+# from django.urls import path, include
+# from rest_framework import routers
 # from .views import ConversationViewSet, MessageViewSet
 
-# router = DefaultRouter()
+# router = routers.DefaultRouter()
 # router.register(r'conversations', ConversationViewSet, basename='conversation')
-
-# convo_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
-# convo_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+# router.register(r'messages', MessageViewSet, basename='message')
 
 # urlpatterns = [
 #     path('', include(router.urls)),
-#     path('', include(convo_router.urls)),
 # ]
+
+
+
+
+from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
+from .views import ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+
+convo_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+convo_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('', include(convo_router.urls)),
+]
