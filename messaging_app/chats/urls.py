929a69a41b1aaa5a2906f41b50afbf64f85f6243
Elkan <Elkan@GALAXY-BOOK>
2025-07-19 09:41:19 -0900

    more patching

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -9,3 +9,20 @@ router.register(r'messages', MessageViewSet, basename='message')
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+
+
+
+# from rest_framework_nested.routers import DefaultRouter, NestedDefaultRouter
+# from .views import ConversationViewSet, MessageViewSet
+
+# router = DefaultRouter()
+# router.register(r'conversations', ConversationViewSet, basename='conversation')
+
+# convo_router = NestedDefaultRouter(router, r'conversations', lookup='conversation')
+# convo_router.register(r'messages', MessageViewSet, basename='conversation-messages')
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('', include(convo_router.urls)),
+# ]
