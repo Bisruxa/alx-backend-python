58ee8ac4014a942efc656245cad28d0d41cfe067
Elkan <Elkan@GALAXY-BOOK>
2025-07-26 11:32:02 -0900

    post man collections

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,9 +1,21 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
 from rest_framework import permissions
+from .models import Conversation
 
-class IsAuthenticatedAndParticipant(BasePermission):
+class IsAuthenticatedAndParticipant(permissions.BasePermission):
     def has_permission(self,request,view):
         return request.user and request.user.is_authenticated
     def has_object_permission(self,request,view,obj):
-        return request.user in obj.participants.all()
+        if not request.user.is_authenticated:
+            return False
+
+        if hasattr(obj, 'conversation'):
+            is_participant = request.user in obj.conversation.participants.all()
+        elif isinstance(obj, Conversation):
+            is_participant = request.user in obj.participants.all()
+        else:
+            return False
+
+        if request.method in ['PUT', 'PATCH', 'DELETE']:
+            return is_participant
+        return True
 
